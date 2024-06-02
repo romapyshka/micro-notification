aa26ac3099ce5590970ecdb19d85793b0b5f3c7e
Roman Pyshka <romasadova@gmail.com>
2024-06-02 21:50:24 +0300

    feat: connected database to server

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   sendNotification(username: string): string {
+    console.log("MASHARAG!")
     return `Notification was send to ${username}!`;
   }
 }
